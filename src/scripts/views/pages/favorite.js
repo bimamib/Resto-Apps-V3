1dbba033dd2eb7e374038e36524370f5376fbd77
bimamib <bimaprast1601@gmail.com>
2023-05-23 16:38:43 +0700

    first commit

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,25 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import { createRestaurantListTemplate } from '../templates/template-creator';
+
+const Favorite = {
+  async render() {
+    return `
+      <div class="content">
+        <h1 class="explore__label">Your Favorite Restaurants</h2>
+        <div id="restaurants" class="restaurants">
+        </div>
+      </div>
+    `;
+  },
+
+  async afterRender() {
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const restaurantsContainer = document.querySelector('#restaurants');
+
+    restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML += createRestaurantListTemplate(restaurant);
+    });
+  },
+};
+
+export default Favorite;
