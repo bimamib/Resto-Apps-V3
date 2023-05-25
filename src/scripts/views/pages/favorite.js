3bd77533b2d0e0ee4ccf909381426adb7f62ab8c
bimamib <bimaprast1601@gmail.com>
2023-05-25 10:30:26 +0700

    update favorite

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -16,7 +16,13 @@ const Favorite = {
     const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
     const restaurantsContainer = document.querySelector('#restaurants');
 
+    if (restaurants.length === 0) {
+      restaurantsContainer.innerHTML = '<p class="restaurant-item__not__found">Tidak ada restaurant untuk ditampilkan</p>';
+    }
+    
     restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML = '';
+    
       restaurantsContainer.innerHTML += createRestaurantListTemplate(restaurant);
     });
   },
